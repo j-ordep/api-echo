042db7683a24051003e9750873899fa1e858acee
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-06 10:29:47 -0300

    feat: add FindByEmail na interface repository

--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -3,6 +3,7 @@ package domain
 type UserRepository interface {
 	CreateUser(user *User) error
 	FindById(id string) (*User, error)
+	FindByEmail(id string) (*User, error)
 	FindAll() ([]*User, error)
 	UpdateUser(user *User) error
 	DeleteById(id string) error
