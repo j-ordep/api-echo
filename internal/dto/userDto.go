294417869edfd1736455de8fd838472843185c17
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-05 23:39:47 -0300

    feat: add DTO -> falta criar metodos e implementar os dtos

---
+++ internal/dto/userDto.go
@@ -0,0 +1,13 @@
+package dto
+
+import "time"
+
+type UserDto struct {
+	Id        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// TODO implementar os DTOs
