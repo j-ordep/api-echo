bf92e51a88405113a8254a4300e0b176247e5b90
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-06 10:30:36 -0300

    feat: add variaveis para errors personalisados

---
+++ internal/domain/errors.go
@@ -0,0 +1,24 @@
+package domain
+
+import "errors"
+
+// User errors
+var (
+	ErrUserNotFound = errors.New("User not found")
+	ErrUserAlreadyExists = errors.New("usuário já existe")
+	ErrInvalidUserData  = errors.New("dados do usuário inválidos")
+	ErrUserIDRequired   = errors.New("ID do usuário é obrigatório")
+)
+
+// General errors
+var (
+    ErrInvalidJSON      = errors.New("JSON inválido")
+    ErrInternalServer   = errors.New("erro interno do servidor")
+)
+
+// HTTP
+var (
+	MsgUserCreated = "Usuário criado com sucesso"
+    MsgUserUpdated = "Usuário atualizado com sucesso"
+    MsgUserDeleted = "Usuário deletado com sucesso"
+)
