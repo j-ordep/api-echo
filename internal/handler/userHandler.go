9975d332c8d6e39e64c5ec9ead54216e6838c845
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-08 22:30:26 -0300

    feat: add logs - handler

--- internal/handler/userHandler.go
+++ internal/handler/userHandler.go
@@ -5,6 +5,7 @@ import (
 	"api-echo/internal/dto"
 	"api-echo/internal/service"
 	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -20,10 +21,13 @@ func NewUserHandler(service *service.UserService) *UserHandler {
 
 func (h *UserHandler) CreateUser(c echo.Context) error {
 
+	log.Println("Handler: recebida requisição para criar usuário")
+
 	var inputDto dto.InputDto
 
 	err := c.Bind(&inputDto)
     if err != nil {
+		log.Println("Handler: erro ao fazer bind do JSON")
         return c.JSON(http.StatusBadRequest, map[string]string{
             "error": domain.ErrInvalidJSON.Error(),
         })
@@ -39,17 +43,20 @@ func (h *UserHandler) CreateUser(c echo.Context) error {
        		 })	
 		}
 
+		log.Println("handler: erro interno ao criar usuário")
         return c.JSON(http.StatusInternalServerError, map[string]string{
             "error": domain.ErrInternalServer.Error(),
         })
     }
+	log.Println("handler: usuário criado com sucesso")
 
 	outputDto := dto.ToResponse(createdUser)
-
     return c.JSON(http.StatusCreated, outputDto)
 }
 
 func (h *UserHandler) FindById(c echo.Context) error {
+	log.Println("Handler: recebida requisição para buscar usuário por ID")
+
   	id := c.Param("id")
 	if id == "" {
         return c.JSON(http.StatusBadRequest, map[string]string {
@@ -64,26 +71,29 @@ func (h *UserHandler) FindById(c echo.Context) error {
                 "error": domain.ErrUserNotFound.Error(),
             })
         }
-
+		log.Println("handler: erro interno ao buscar usuário")
     	return c.JSON(http.StatusInternalServerError, map[string]string{
         	"error": domain.ErrInternalServer.Error(),
         })
     }
+	log.Println("Handler: usuário encontrado")
 
 	output := dto.ToResponse(user) 
-
 	return c.JSON(http.StatusOK, output)
 }
 
 func (h *UserHandler) FindAll(c echo.Context) error {
+	log.Println("handler: recebida requisição para buscar todos os usuários")
 
 	users, err := h.service.FindAll()
 	if err != nil {
+		log.Println("handler: erro ao buscar todos os usuarios")
 		return c.JSON(http.StatusInternalServerError, map[string]string {
 			"error": domain.ErrInternalServer.Error(),
 		})
 	}
 
+	log.Println("handler: usuários encontrados")
 	var outputDtos []*dto.OutputDto
     for _, user := range users {
         outputDtos = append(outputDtos, dto.ToResponse(user))
@@ -93,6 +103,8 @@ func (h *UserHandler) FindAll(c echo.Context) error {
 }
 
 func (h *UserHandler) UpdateUser(c echo.Context) error {
+	log.Println("handler: recebida requisição para atualizar usuário")
+
 	id := c.Param("id")
 	if id == "" {
 		return c.JSON(http.StatusBadRequest, map[string]string {
@@ -104,6 +116,7 @@ func (h *UserHandler) UpdateUser(c echo.Context) error {
 
 	err := c.Bind(&inputDto)
 	if err != nil {
+		log.Println("handler: erro ao fazer bind do JSON")
         return c.JSON(http.StatusBadRequest, map[string]string{
             "error": domain.ErrInvalidJSON.Error(),
         })
@@ -113,17 +126,21 @@ func (h *UserHandler) UpdateUser(c echo.Context) error {
 
 	domainUser, err := h.service.UpdateUser(id, user)
 	if err != nil {
+		log.Println("handler: erro ao atualizar usuario")
 		return c.JSON(http.StatusInternalServerError, map[string]string {
 			"error": domain.ErrInternalServer.Error(), 
 		})
 	}
 
-	outputDto := dto.ToResponse(domainUser)
+	log.Println("handler: usuário atualizado com sucesso")
 
+	outputDto := dto.ToResponse(domainUser)
 	return c.JSON(http.StatusOK, outputDto)
 }
 
 func (h *UserHandler) DeleteById(c echo.Context) error {
+	log.Println("handler: recebida requisição para deletar usuário")
+
 	id := c.Param("id")
 	if id == "" {
 		return c.JSON(http.StatusBadRequest, map[string]string {
@@ -133,10 +150,13 @@ func (h *UserHandler) DeleteById(c echo.Context) error {
 
 	err := h.service.DeleteById(id)
     if err != nil {
+		log.Println("handler: erro ao deletar usuário")
         return c.JSON(http.StatusInternalServerError, map[string]string{
             "error": domain.ErrInternalServer.Error(),
         })
     }
+
+	log.Println("handler: usuário deletado com sucesso")
 
 	return c.JSON(http.StatusOK, map[string]string {
     	"message": domain.MsgUserDeleted,
