567974273028477d5a791b227bd294d7f9fefe24
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-09 00:41:03 -0300

    feat: add gorm repository

--- internal/repository/userRespository.go
+++ internal/repository/userRespository.go
@@ -2,153 +2,56 @@ package repository
 
 import (
 	"api-echo/internal/domain"
-	"database/sql"
-	"time"
+	"gorm.io/gorm"
 )
 
 type UserRepository struct {
-	db *sql.DB
+	db *gorm.DB
 }
 
-func NewUserRepository(db *sql.DB) *UserRepository {
+func NewUserRepository(db *gorm.DB) *UserRepository {
 	return &UserRepository{db: db}
 }
 
 func (r *UserRepository) CreateUser(user *domain.User) error {
-	stmt, err := r.db.Prepare(`
-		INSERT INTO users (id, name, email, created_at, updated_at)
-        VALUES ($1, $2, $3, $4, $5)
-	`)
-	if err != nil {
-		return err
-	}
-
-	defer stmt.Close()
-
-	_, err = stmt.Exec(
-		user.Id,
-		user.Name,
-		user.Email,
-		user.CreatedAt,
-		user.UpdatedAt,
-	)
-	if err != nil {
-		return err
-	}
-	return nil
+    return r.db.Create(user).Error
 }
 
 func (r *UserRepository) FindById(id string) (*domain.User, error) {
-	var user domain.User
-	var createdAt, updatedAt time.Time
-
-	err := r.db.QueryRow(`
-		SELECT id, name, email, created_at, updated_at
-		FROM users
-		WHERE id = $1
-	`, id).Scan(
-		&user.Id,
-		&user.Name,
-		&user.Email,
-		&createdAt,
-		&updatedAt,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	user.CreatedAt = createdAt
-	user.UpdatedAt = updatedAt
-	return &user, nil
+    var user domain.User
+    result := r.db.First(&user, "id = ?", id)
+    if result.Error != nil {
+        return nil, result.Error
+    }
+    return &user, nil
 }
 
 func (r *UserRepository) FindByEmail(email string) (*domain.User, error) {
-	var user domain.User
-	var createdAt, updatedAt time.Time
-
-	err := r.db.QueryRow(`
-		SELECT id, name, email, created_at, updated_at
-		FROM users
-		WHERE email = $1
-	`, email).Scan(
-		&user.Id,
-		&user.Name,
-		&user.Email,
-		&createdAt,
-		&updatedAt,
-	)
-	if err == sql.ErrNoRows {
-		return nil, domain.ErrUserNotFound
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	user.CreatedAt = createdAt
-	user.UpdatedAt = updatedAt
-	return &user, nil
+    var user domain.User
+    result := r.db.First(&user, "email = ?", email)
+    if result.Error != nil {
+        if result.Error == gorm.ErrRecordNotFound {
+            return nil, domain.ErrUserNotFound
+        }
+        return nil, result.Error
+    }
+    return &user, nil
 }
 
 func (r *UserRepository) FindAll() ([]*domain.User, error) {
-	rows, err := r.db.Query(`SELECT id, name, email, created_at, updated_at FROM users`)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var users []*domain.User
-
-	for rows.Next() {
-		var user domain.User
-		var createdAt, updatedAt time.Time
-
-		err := rows.Scan(
-			&user.Id,
-			&user.Name,
-			&user.Email,
-			&createdAt,
-			&updatedAt,
-		)
-		if err != nil {
-			return nil, err
-		}
-		user.CreatedAt = createdAt
-		user.UpdatedAt = updatedAt
-
-		users = append(users, &user)
-	}
-
-	if err = rows.Err(); err != nil {
-        return nil, err
+    var users []*domain.User
+    result := r.db.Find(&users)
+    if result.Error != nil {
+        return nil, result.Error
     }
-
     return users, nil
 }
 
 func (r *UserRepository) UpdateUser(user *domain.User) error {
-	_, err := r.db.Exec(`
-		UPDATE users 
-		SET name = $1, email = $2, updated_at = $3
-		WHERE id = $4
-	`,
-		user.Name, 
-		user.Email, 
-		time.Now(),
-		user.Id,
-	)
-	if err != nil {
-		return err
-	}
-
-	return nil
+    return r.db.Save(user).Error
 }
 
 func (r *UserRepository) DeleteById(id string) error {
-	_, err := r.db.Exec(`DELETE FROM users WHERE id = $1`, id)
-	if err != nil {
-		return err
-	}
-
-	return nil
+    result := r.db.Delete(&domain.User{}, "id = ?", id)
+    return result.Error
 }
